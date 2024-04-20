03a63468b36e6a5ef1fade67abf1793de862afdc
Chetan-KK <chetankhulage350@gmail.com>
2024-04-20 09:41:02 +0530

    init

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Sectioner UI</span>,
+  project: {
+    link: "https://github.com/chetan-kk/Sectioner-UI",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/chetan-kk/Sectioner-UI",
+  footer: {
+    text: "Sectioner UI",
+  },
+};
+
+export default config;
