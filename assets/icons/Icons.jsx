3026cb2b58e2a9113648ea215d3ea038ef49911e
Chetan-KK <chetankhulage350@gmail.com>
2024-04-21 11:20:43 +0530

    fix:redirect

--- assets/icons/Icons.jsx
+++ assets/icons/Icons.jsx
@@ -21,7 +21,7 @@ export const ReactjsIcon = () => {
 export const JsIcon = () => {
   return (
     <svg
-      class="svg-icon"
+      className="svg-icon"
       viewBox="0 0 1024 1024"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
