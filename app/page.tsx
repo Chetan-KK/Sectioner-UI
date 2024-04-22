46702f5ebdbaf8084a3cb6d4fa5c3a3a107557e3
Chetan-KK <chetankhulage350@gmail.com>
2024-04-22 14:17:56 +0530

    fix: steps

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client"
+
+import { useTheme } from "next-themes";
 import Link from "next/link";
 
 export default function Home() {
+
+  const {setTheme,resolvedTheme} = useTheme()
+  
+  function toggleTheme(){
+    if(resolvedTheme=='light')
+      {
+        setTheme('dark')
+      }
+      else{
+        setTheme("light")
+      }
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={toggleTheme}>Toggle</button>
       <Link href={'/docs/introduction'}>docs</Link>
     </main>
   );
